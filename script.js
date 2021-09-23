2dba027d523148e7c58291873a4d914f0015d354
kanishths <kanishthasiyaram15@gmail.com>
2021-09-23 18:17:06 +0530

    changes made

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ btn.addEventListener("click", () => {
     form.style.opacity = "0";
   }
 });
+
+const submit = document.querySelector("#btn-sub");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const answer = document.querySelector("#q").value;
+
+  if (answer === "ans1") {
+    window.location.hash =
+      "https://online-aaruush.github.io/trapped/hashxy.html";
+  }
+  if (answer === "ans2") {
+    window.location.hash =
+      "https://online-aaruush.github.io/trapped/finalhash.html";
+  } else {
+    alert("Wrong answer, please try again!! :(");
+  }
+});
